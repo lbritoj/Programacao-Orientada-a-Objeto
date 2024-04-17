de5e772892cf317ba2747e5c35eb3af061d192d9
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-04-16 22:32:37 -0400

    first commit

---
+++ Aula_1/src/Exercicio_1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercicio_1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Digite seu peso:");
+		int peso = teclado.nextInt();
+		System.out.println("Digite sua altura:");
+		double altura = teclado.nextDouble();
+		double imc = peso / (altura * altura);
+		System.out.println("Seu IMC é " + imc);
+		
+		
+	}
+	
+	
+	
+	}
+
